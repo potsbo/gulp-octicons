a2eb925e24f571dc7f18fc317775214eeea2b6a9
Shimpei Otsubo <potsbo@gmail.com>
2017-01-18 05:43:35 +0900

    Use arrow function

--- test/index.js
+++ test/index.js
@@ -5,8 +5,8 @@ chai.use(chaiFiles)
 const file = chaiFiles.file
 const expect = chai.expect
 
-describe('gulp-octicons', function() {
-  describe('pipe', function() {
+describe('gulp-octicons', () => {
+  describe('pipe', () => {
     function itShouldRenderExpectedFile(suffix) {
       const actual = file(`test/dest/index-${suffix}.html`)
       const expected = file(`test/expected/index-${suffix}.html`)
@@ -14,18 +14,18 @@ describe('gulp-octicons', function() {
     }
     const gulp = require('gulp')
     const gulpOcticons = require('../index')
-    gulp.task('octicons', function() {
+    gulp.task('octicons', () => {
       gulp.src('test/fixtures/*.html')
         .pipe(gulpOcticons())
         .pipe(gulp.dest('./test/dest'))
     })
     gulp.run('octicons')
 
-    it('should render expected html file', function() {
+    it('should render expected html file', () => {
       itShouldRenderExpectedFile('basic')
     })
 
-    it('should respond to more than one octicons', function() {
+    it('should respond to more than one octicons', () => {
       itShouldRenderExpectedFile('multiple')
     })
   })
