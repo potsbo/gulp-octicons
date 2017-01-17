e8bacfb225e403740479d00f541e4e081f52020c
Shimpei Otsubo <potsbo@gmail.com>
2017-01-18 05:47:26 +0900

    Use arrow function

--- test/lib/octicons-replace.js
+++ test/lib/octicons-replace.js
@@ -6,43 +6,41 @@ $ = {
 }
 
 const strs = {
-  open: function(option = {}){
+  open: (option = {}) => {
     const iconName = option.iconName || 'x'
     return `<!-- octicons:${iconName} -->`
   },
   close: '<!-- endocticons -->',
 }
-function svg(iconName = 'x') {
+const svg = (iconName = 'x') => {
   return octicons[iconName].toSVG()
 }
 
 function itShouldHaveCorrectIndent(str, expectedBase) {
-  describe('indent', function() {
-    it('should insert same indent as openComment', function() {
+  describe('indent', () => {
+    it('should insert same indent as openComment', () => {
       const indented = `  ${str}`
       const actual = $.octicons.replace(indented)
-      const expected = expectedBase.map(function (block) {
-        return `  ${block}`
-      }).join('\n')
+      const expected = expectedBase.map((block) => `  ${block}`).join('\n')
       expect(actual).to.eql(expected)
     })
   })
 }
 
-describe('gulp-octicons', function() {
-  describe('replace', function() {
+describe('gulp-octicons', () => {
+  describe('replace', () => {
     const str = [strs.open(), strs.close].join('\n')
     const expectedBase = [strs.open(), svg(), strs.close]
-    it('should match and replace', function() {
+    it('should match and replace', () => {
       const actual = $.octicons.replace(str)
       const expected = expectedBase.join('\n')
       expect(actual).to.eql(expected)
     })
-    context('when not registered icon specified', function() {
+    context('when not registered icon specified', () => {
       const open = strs.open({ iconName: 'not-registered-icon-name' })
       const str = [open, strs.close].join('\n')
       const expectedBase = [open, strs.close]
-      it('should not insert anything', function() {
+      it('should not insert anything', () => {
         const actual = $.octicons.replace(str)
         const expected = expectedBase.join('\n')
         expect(actual).to.eql(expected)
@@ -51,17 +49,17 @@ describe('gulp-octicons', function() {
     })
     itShouldHaveCorrectIndent(str, expectedBase)
 
-    context('when more than one octions given', function() {
+    context('when more than one octions given', () => {
       const arrows = ['arrow-down', 'arrow-left', 'arrow-right', 'arrow-up']
-      const comments = arrows.map(function(d) {
+      const comments = arrows.map((d) => {
         return [strs.open({ iconName: d }), strs.close].join('\n')
       })
-      const expectedBases = arrows.map(function(d) {
+      const expectedBases = arrows.map((d) => {
         return [strs.open({ iconName: d}), svg(d), strs.close]
       })
       const expectedBase = [].concat.apply([], expectedBases)
       const str = comments.join('\n')
-      it('should match and replace', function() {
+      it('should match and replace', () => {
         const actual = $.octicons.replace(str)
         const expected = expectedBase.join('\n')
         expect(actual).to.eql(expected)
