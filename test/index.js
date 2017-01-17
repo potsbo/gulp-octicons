d7b28618f0fbb8bd76aea08a3997d9c54dee8f3c
Shimpei Otsubo <potsbo@gmail.com>
2017-01-18 02:43:35 +0900

    Use expectedBase

--- test/index.js
+++ test/index.js
@@ -11,16 +11,17 @@ const strs = {
 describe('gulp-octicons', function() {
   describe('replace', function() {
     const str = [strs.open, strs.close].join('\n')
+    const expectedBase = [strs.open, 'icon-name.path', strs.close]
     it('should match and replace', function() {
       const actual = $.octicons.replace(str)
-      const expected = [strs.open, 'icon-name.path', strs.close].join('\n')
+      const expected = expectedBase.join('\n')
       expect(actual).to.eql(expected)
     })
     describe('indent', function() {
       it('should insert same indent as openComment', function() {
         const indented = `  ${str}`
         const actual = $.octicons.replace(indented)
-        const expected = "  <!-- octicon:icon-name -->\n  icon-name.path\n  <!-- endocticon -->"
+        const expected = expectedBase.map(function(block){ return `  ${block}`}).join('\n')
         expect(actual).to.eql(expected)
       })
     })
