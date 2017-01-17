9577cc48d89971509e389d8814039d242644a517
Shimpei Otsubo <potsbo@gmail.com>
2017-01-18 05:41:20 +0900

    Separate files

--- test/index.js
+++ test/index.js
@@ -33,45 +33,6 @@ function itShouldHaveCorrectIndent(str, expectedBase) {
 }
 
 describe('gulp-octicons', function() {
-  describe('replace', function() {
-    const str = [strs.open(), strs.close].join('\n')
-    const expectedBase = [strs.open(), svg(), strs.close]
-    it('should match and replace', function() {
-      const actual = $.octicons.replace(str)
-      const expected = expectedBase.join('\n')
-      expect(actual).to.eql(expected)
-    })
-    context('when not registered icon specified', function() {
-      const open = strs.open({ iconName: 'not-registered-icon-name' })
-      const str = [open, strs.close].join('\n')
-      const expectedBase = [open, strs.close]
-      it('should not insert anything', function() {
-        const actual = $.octicons.replace(str)
-        const expected = expectedBase.join('\n')
-        expect(actual).to.eql(expected)
-      })
-      itShouldHaveCorrectIndent(str, expectedBase)
-    })
-    itShouldHaveCorrectIndent(str, expectedBase)
-
-    context('when more than one octions given', function() {
-      const arrows = ['arrow-down', 'arrow-left', 'arrow-right', 'arrow-up']
-      const comments = arrows.map(function(d) {
-        return [strs.open({ iconName: d }), strs.close].join('\n')
-      })
-      const expectedBases = arrows.map(function(d) {
-        return [strs.open({ iconName: d}), svg(d), strs.close]
-      })
-      const expectedBase = [].concat.apply([], expectedBases)
-      const str = comments.join('\n')
-      it('should match and replace', function() {
-        const actual = $.octicons.replace(str)
-        const expected = expectedBase.join('\n')
-        expect(actual).to.eql(expected)
-      })
-    })
-  })
-
   describe('pipe', function() {
     function itShouldRenderExpectedFile(suffix) {
       const actual = file(`test/dest/index-${suffix}.html`)
---
+++ test/lib/octicons-replace.js
@@ -0,0 +1,71 @@
+const octicons = require('octicons')
+const chai = require('chai')
+const expect = chai.expect
+$ = {
+  octicons: require('../../lib/octicons-replace')
+}
+
+const strs = {
+  open: function(option = {}){
+    const iconName = option.iconName || 'x'
+    return `<!-- octicons:${iconName} -->`
+  },
+  close: '<!-- endocticons -->',
+}
+function svg(iconName = 'x') {
+  return octicons[iconName].toSVG()
+}
+
+function itShouldHaveCorrectIndent(str, expectedBase) {
+  describe('indent', function() {
+    it('should insert same indent as openComment', function() {
+      const indented = `  ${str}`
+      const actual = $.octicons.replace(indented)
+      const expected = expectedBase.map(function (block) {
+        return `  ${block}`
+      }).join('\n')
+      expect(actual).to.eql(expected)
+    })
+  })
+}
+
+describe('gulp-octicons', function() {
+  describe('replace', function() {
+    const str = [strs.open(), strs.close].join('\n')
+    const expectedBase = [strs.open(), svg(), strs.close]
+    it('should match and replace', function() {
+      const actual = $.octicons.replace(str)
+      const expected = expectedBase.join('\n')
+      expect(actual).to.eql(expected)
+    })
+    context('when not registered icon specified', function() {
+      const open = strs.open({ iconName: 'not-registered-icon-name' })
+      const str = [open, strs.close].join('\n')
+      const expectedBase = [open, strs.close]
+      it('should not insert anything', function() {
+        const actual = $.octicons.replace(str)
+        const expected = expectedBase.join('\n')
+        expect(actual).to.eql(expected)
+      })
+      itShouldHaveCorrectIndent(str, expectedBase)
+    })
+    itShouldHaveCorrectIndent(str, expectedBase)
+
+    context('when more than one octions given', function() {
+      const arrows = ['arrow-down', 'arrow-left', 'arrow-right', 'arrow-up']
+      const comments = arrows.map(function(d) {
+        return [strs.open({ iconName: d }), strs.close].join('\n')
+      })
+      const expectedBases = arrows.map(function(d) {
+        return [strs.open({ iconName: d}), svg(d), strs.close]
+      })
+      const expectedBase = [].concat.apply([], expectedBases)
+      const str = comments.join('\n')
+      it('should match and replace', function() {
+        const actual = $.octicons.replace(str)
+        const expected = expectedBase.join('\n')
+        expect(actual).to.eql(expected)
+      })
+    })
+  })
+})
