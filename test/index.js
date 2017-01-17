47f4aee1b4a9334b99a4e40aafc60b6f358cc7f3
Shimpei Otsubo <potsbo@gmail.com>
2017-01-18 02:33:22 +0900

    Refactor: reuse str

--- test/index.js
+++ test/index.js
@@ -5,16 +5,16 @@ $ = {
 
 describe('gulp-octicons', function() {
   describe('replace', function() {
+    const str = "<!-- octicon:icon-name -->\n<!-- endocticon -->"
     it('should match and replace', function() {
-      const str = "<!-- octicon:icon-name -->\n<!-- endocticon -->"
       const actual = $.octicons.replace(str)
       const expected = "<!-- octicon:icon-name -->\nicon-name.path\n<!-- endocticon -->"
       expect(actual).to.eql(expected)
     })
     describe('indent', function() {
       it('should insert same indent as openComment', function() {
-        const str = "  <!-- octicon:icon-name -->\n<!-- endocticon -->"
-        const actual = $.octicons.replace(str)
+        const indented = `  ${str}`
+        const actual = $.octicons.replace(indented)
         const expected = "  <!-- octicon:icon-name -->\n  icon-name.path\n  <!-- endocticon -->"
         expect(actual).to.eql(expected)
       })
